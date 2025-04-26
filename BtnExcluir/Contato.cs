fa56f8d369cbe1eb3336cfb0cfc02765b0839f8a
Matheus de Almeida Cantarutti <81988636+cantaruttim@users.noreply.github.com>
2025-04-25 22:08:26 -0300

    Classe Contato - Exemplo

---
+++ BtnExcluir/Contato.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BtnExcluir
+{
+    internal class Contato
+    {
+        public int id;
+        public string nome;
+
+
+        public Contato(int id, string nome)
+        {
+            this.id = id;
+            this.nome = nome;
+        }
+
+        public Contato() { }
+
+    }
+}
